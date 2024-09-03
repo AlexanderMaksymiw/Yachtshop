de640e340b1fdf952677f5306bc3dfa3d9ad9ab5
Arran4567 <Arran4567@gmail.com>
2024-09-03 20:57:10 +0100

    Removing User setup

--- AlexAPI/Data/DbInitializer.cs
+++ AlexAPI/Data/DbInitializer.cs
@@ -26,39 +26,6 @@ namespace AlexAPI.Data
 
         public void Initialize()
         {
-            string superadminEmail = _configuration.GetValue<string>("SuperUser:Email");
-            string superadminUsername = _configuration.GetValue<string>("SuperUser:Username");
-            string superadminPassword = _configuration.GetValue<string>("SuperUser:Password");
-            string superadminDefaultRole = _configuration.GetValue<string>("SuperUser:Role");
-            ApplicationUser? user = _userManager.FindByEmailAsync(superadminEmail).Result;
-            if (user == null)
-            {
-                var authenticator = new Google.Authenticator.TwoFactorAuthenticator();
-
-                user = new ApplicationUser
-                {
-                    UserName = superadminUsername,
-                    Email = superadminEmail,
-                    EmailConfirmed = true,
-                    secretKey2FA = Guid.NewGuid()
-                };
-                _ = _userManager.CreateAsync(user, superadminPassword).Result;
-            }
-
-            foreach (var r in _configuration.GetSection("DefaultRoles").Get<string[]>())
-            {
-                var newRole = new IdentityRole
-                {
-                    Name = r
-                };
-                _ = _roleManager.CreateAsync(newRole).Result;
-                _ = _userManager.AddToRoleAsync(user, r).Result;
-            }
-
-            if (!_userManager.GetRolesAsync(user).Result.Contains(superadminDefaultRole))
-            {
-                _ = _userManager.AddToRoleAsync(user, superadminDefaultRole).Result;
-            }
         }
     }
 }
