b7f219f0351a641889c10d495474ff9b6ea606fe
Arran4567 <Arran4567@gmail.com>
2024-10-31 13:30:14 +0000

    Fix for subtype infinite loop

--- AlexAPI/Models/SubType.cs
+++ AlexAPI/Models/SubType.cs
@@ -1,9 +1,12 @@
-﻿namespace AlexAPI.Models
+﻿using System.Text.Json.Serialization;
+
+namespace AlexAPI.Models
 {
     public class SubType
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Specification> Specifications { get; set; }
     }
 }
