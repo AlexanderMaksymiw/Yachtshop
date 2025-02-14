60b97816b9f88606a1528dc1a2027967fce2c0c0
alex maksymiw <amaksymiw02@gmail.com>
2025-02-14 16:59:52 +0000

    Fixed runtime error

--- AlexAPI/Authentication/RegisterModel.cs
+++ AlexAPI/Authentication/RegisterModel.cs
@@ -12,6 +12,5 @@ namespace AlexAPI.Authentication
         public string? Email { get; set; }
         [Required(ErrorMessage = "Password is required")]
         public string? Password { get; set; }
-        [Required(ErrorMessage = "Role is required")]
     }
 }
