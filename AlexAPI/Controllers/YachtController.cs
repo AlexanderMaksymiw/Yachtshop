6f766cc29f9ed34317df0e84ce1768fbc99c6075
Arran Jones <Arran4567@gmail.com>
2025-04-15 18:47:11 +0100

    Updated Get

--- AlexAPI/Controllers/YachtController.cs
+++ AlexAPI/Controllers/YachtController.cs
@@ -103,7 +103,8 @@ namespace AlexAPI.Controllers
             string? hullType = null,
             string? builder = null,
             string[]? equipment = null,
-            bool? onSale = null
+            bool? onSale = null,
+            string? location = null
         )
         {
             try
@@ -133,7 +134,8 @@ namespace AlexAPI.Controllers
                     (hullType == null || x.Specification.HullType == hullType) &&
                     (builder == null || x.Specification.Builder == builder) &&
                     (equipment == null || equipment.All(e => x.Amenities.Equipment.Select(a => a.Name).Contains(e))) &&
-                    (onSale == null || x.OnSale == onSale);
+                    (onSale == null || x.OnSale == onSale) &&
+                    (location == null || x.Locations.Any(x => x.Name == location));
 
                 // Apply pagination and execute query
                 var result = workUnit.YachtRepository
