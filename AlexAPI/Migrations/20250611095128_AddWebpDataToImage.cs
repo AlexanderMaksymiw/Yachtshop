00fdba88a5775f7ef2ef5046fe5672b04151058b
AlexanderMaksymiw <amaksymiw02@gmail.com>
2025-06-11 10:55:05 +0100

    migration for webp image for database

---
+++ AlexAPI/Migrations/20250611095128_AddWebpDataToImage.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlexAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWebpDataToImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "WebpData",
+                table: "Images",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WebpData",
+                table: "Images");
+        }
+    }
+}
