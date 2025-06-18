95bac1da1cfd58456e68ae2ae5451e4fda0e1479
AlexanderMaksymiw <amaksymiw02@gmail.com>
2025-06-18 16:01:16 +0100

    add logging

--- AlexAPI/Models/Yacht.cs
+++ AlexAPI/Models/Yacht.cs
@@ -7,7 +7,7 @@ namespace AlexAPI.Models
         [Key]
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public string? SYTUrl { get; set; }
         public decimal? Price { get; set; }
         public bool OnSale { get; set; }
